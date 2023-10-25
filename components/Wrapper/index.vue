201e06e36ae06e56f6886d629fbad35230aa32d3
Zzhgitup <99540215+Zzhgitup@users.noreply.github.com>
2023-10-25 20:06:54 +0800

    feat:小改

---
+++ components/Wrapper/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="wrapperMixd">
+    <div class="mixed">
+      <div class="mixedinner">
+        <div class="header-title">未来软件工作室</div>
+        <div class="header-title EN">Future software Studio</div>
+        <div class="header-title EN2">做一个有影响力的人</div>
+        <div class="header-title EN3">做一个有影响力的团队</div>
+        <div class="header-title EN4">
+          <TransitionButton innertext="了解更多"></TransitionButton>
+        </div>
+      </div>
+    </div>
+    <div class="wrapper">
+      <el-carousel height="100vh" :interval="5000" arrow="always">
+        <el-carousel-item v-for="(item, index) in carouselItem" :key="index">
+          <img height="100%" width="100%" :src="item.src" alt="" />
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const carouselItem = [
+  {
+    src: "/images/wallhaven-3l2vm3_1920x1080.png",
+  },
+  {
+    src: "/images/wallhaven-kxj3l1_1920x1080.png",
+  },
+  {
+    src: "/images/wallhaven-vqeyxl_3840x2160.png",
+  },
+  {
+    src: "/images/wallhaven-yxkm5k_3840x2160.png",
+  },
+];
+</script>
+<style scoped lang="scss">
+.wrapperMixd {
+  width: 100%;
+  height: auto;
+  position: relative;
+  .wrapper {
+    height: 120vh;
+    width: 100%;
+  }
+  .mixed {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100vh;
+    padding-top: 1.8rem;
+    z-index: 1;
+    display: flex;
+    justify-content: center;
+    background-color: rgba($color: #000000, $alpha: 0.1);
+    .mixedinner {
+      width: 10.6rem;
+      background-color: transparent;
+      margin: 0 auto;
+      padding-top: 1rem;
+      .header-title {
+        cursor: pointer;
+        font-size: 0.6rem;
+        color: rgb(255, 255, 255);
+        font-weight: 700;
+        text-align: start;
+      }
+      .EN {
+        margin-top: 0.2rem;
+        font-size: 0.3rem;
+        color: rgb(33, 155, 255);
+      }
+      .EN2 {
+        margin-top: 0.2rem;
+        font-size: 0.2rem;
+        color: rgb(255, 255, 255);
+      }
+      .EN3 {
+        margin-top: 0.2rem;
+        font-size: 0.2rem;
+        color: rgb(255, 255, 255);
+      }
+      .EN4 {
+        margin-top: 1rem;
+        font-size: 0.2rem;
+        width: 2rem;
+        height: 0.5rem;
+        color: rgb(255, 255, 255);
+      }
+    }
+  }
+}
+</style>
--- components/app-header/index.vue
+++ components/app-header/index.vue
@@ -113,8 +113,7 @@
         </div>
       </nav>
       <div class="login">
-        <a class="loginbt">进入后台</a>
-        <div class=""></div>
+        <TransitionButton innertext="进入后台"></TransitionButton>
       </div>
     </div>
   </div>
@@ -174,6 +173,7 @@ function handlerscroll() {
   transition-duration: 0.3s;
   font-size: 0.16rem;
   color: white;
+  z-index: 999;
   .container {
     position: relative;
     width: 10.6rem;
@@ -383,53 +383,6 @@ function handlerscroll() {
       display: flex;
       align-items: center;
       justify-content: center;
-      .loginbt {
-        align-items: center;
-        background-color: #284dd5;
-        border: 1px solid #284dd5;
-        border-radius: 4px;
-        color: #fff;
-        cursor: pointer;
-        display: flex;
-        justify-content: center;
-        line-height: 1;
-        padding: 0.12rem 0.24rem;
-        overflow: hidden;
-        position: relative;
-        transition: all 0.3s ease; // 将所有属性的过渡效果设置为0.3秒，使用ease缓动函数
-        &:hover {
-          box-shadow: 0.08rem 0.08rem 0.2rem 0 rgba(0, 0, 0, 0.24);
-          transform: translateY(-3px);
-          &::before {
-            left: 100%; // 修复伪元素位置
-            visibility: visible; // 修复伪元素可见性
-          }
-        }
-        &::before {
-          background: linear-gradient(
-            90deg,
-            hsla(0, 0%, 100%, 0) 0,
-            hsla(0, 0%, 100%, 0.03) 1%,
-            hsla(0, 0%, 100%, 0.6) 30%,
-            hsla(0, 0%, 100%, 0.85) 50%,
-            hsla(0, 0%, 100%, 0.85) 70%,
-            hsla(0, 0%, 100%, 0.85) 71%,
-            hsla(0, 0%, 100%, 0)
-          );
-          bottom: auto;
-          content: "";
-          height: 2.2rem;
-          left: -1rem;
-          opacity: 0.1;
-          position: absolute;
-          right: auto;
-          top: -0.4rem;
-          transform: skew(-12deg, 0deg);
-          transition: all 0.5s ease 0s; // 将所有属性的过渡效果设置为0.5秒，使用ease缓动函数
-          width: 0.5rem;
-          z-index: -1;
-        }
-      }
     }
   }
 }
