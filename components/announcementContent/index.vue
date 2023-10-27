88c5b1292ff38877248f587dc7ef935b97b90341
zll <zll>
2023-10-27 08:38:51 +0800

    ✨ feat: 公告模板

---
+++ components/announcementContent/index.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="announcement">
+        <div class="content">
+            <el-timeline>
+                <el-timeline-item center placement="top" :timestamp="timestamp">
+                    <el-card>
+                        <h2>{{ title }}</h2>
+                        <p>{{ content }}</p>
+                    </el-card>
+                </el-timeline-item>
+                <el-timeline-item center timestamp="2018/4/12" placement="top">
+                    <el-card>
+                        <h4>Update Github template</h4>
+                        <p>Tom committed 2018/4/12 20:46</p>
+                    </el-card>
+                </el-timeline-item>
+                <el-timeline-item center timestamp="2018/4/12" placement="top">
+                    <el-card>
+                        <h4>Update Github template</h4>
+                        <p>Tom committed 2018/4/12 20:46</p>
+                    </el-card>
+                </el-timeline-item>
+                <el-timeline-item center timestamp="2018/4/12" placement="top">
+                    <el-card>
+                        <h4>Update Github template</h4>
+                        <p>Tom committed 2018/4/12 20:46</p>
+                    </el-card>
+                </el-timeline-item>
+                <el-timeline-item center timestamp="2018/4/12" placement="top">
+                    <el-card>
+                        <h4>Update Github template</h4>
+                        <p>Tom committed 2018/4/12 20:46</p>
+                    </el-card>
+                </el-timeline-item>
+                <el-timeline-item center timestamp="2018/4/12" placement="top">
+                    <el-card>
+                        <h4>Update Github template</h4>
+                        <p>Tom committed 2018/4/12 20:46</p>
+                    </el-card>
+                </el-timeline-item>
+                <el-timeline-item center timestamp="2018/4/12" placement="top">
+                    <el-card>
+                        <h4>Update Github template</h4>
+                        <p>Tom committed 2018/4/12 20:46</p>
+                    </el-card>
+                </el-timeline-item>
+
+            </el-timeline>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+export interface IProps {
+    content:string,
+    timestamp: string,
+    size?: string,
+    type?:string,
+    icon?: string,
+    title?: string,
+}
+
+
+
+const Props = withDefaults(defineProps<IProps>(),{
+    content:'Custom',
+    timestamp: '2018/4/17',
+    title: '考核通知'
+})
+
+
+</script>
+
+<style scoped lang="scss">
+.announcement{
+    overflow: hidden;
+    padding: 24px;
+    width: 50%;
+    height: 600px ;
+    display: flex;
+    flex-direction: column;
+    border: 1px solid #e3e8f7;
+    border-radius: 12px;
+    background: white;
+    .content{
+        width:100%;
+        height: 100%;
+        overflow-y: auto;
+    }
+    .content::-webkit-scrollbar{
+        width:0px
+    }
+
+}
+</style>
---
+++ layouts/announcement.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="announcementl">
+        <Content></Content>
+        <slot></slot>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Content from '@/components/announcementContent/index.vue'
+
+interface IProps {
+    title: string
+}
+const props = withDefaults(defineProps<IProps>(),{
+    title:'公告'
+})
+
+useHead({
+    title:props.title
+})
+
+
+</script>
+
+<style scoped lang="scss">
+.announcementl{
+    width: 100%;
+    height: 100vh;
+    background-color: rgba(247,249,254,1);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+</style>
