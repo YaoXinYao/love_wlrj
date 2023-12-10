b21c618ed580e589db1075e3177d0ed719f2b3b0
Zzhgitup <99540215+Zzhgitup@users.noreply.github.com>
2023-12-10 09:06:54 +0800

    📃 docs: 压缩代码

--- service/rotation.ts
+++ service/rotation.ts
@@ -2,14 +2,14 @@ import hyRequest from "./forum";
 //查看轮播图
 export const selectCarousel = () => {
   return hyRequest.request(
-    "/zinfo/user/swagger/carousel/selectAllCarousel",
+    "/api/user/swagger/user/carousel/selectAllCarousel",
     "GET"
   );
 };
 //删除轮播图
 export const deleteCarousel = (carouselId: number) => {
   return hyRequest.request(
-    "/zinfo/user/swagger/carousel/deleteCarousel",
+    "/api/user/swagger/user/carousel/deleteCarousel",
     "DELETE",
     { carouselId }
   );
@@ -17,7 +17,7 @@ export const deleteCarousel = (carouselId: number) => {
 //添加轮播
 export const addCarousel = (query: any, params: FormData) => {
   return hyRequest.request(
-    "/zinfo/user/swagger/carousel/insertCarousel",
+    "/api/user/swagger/user/carousel/insertCarousel",
     "POST",
     query,
     {
