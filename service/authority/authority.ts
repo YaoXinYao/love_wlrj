daf71dabb258e9f047437ca154ca60cba03b8769
yexi <2794957189@qq.com>
2023-12-10 08:29:16 +0800

    🐞 fix:修改路径

--- service/authority/authority.ts
+++ service/authority/authority.ts
@@ -1,62 +1,62 @@
 import hyRequest from "../forum";
 //获取所有的角色
 export const selectAllRole = () => {
-  return hyRequest.request("/api/forum/user/role/selectAllRole", "GET");
+  return hyRequest.request("/api/user/role/selectAllRole", "GET");
 };
 //获取角色的人数
 export const getUserNum = (roleId: number) => {
-  return hyRequest.request("/api/forum/user/role/getUserNum", "GET", { roleId });
+  return hyRequest.request("/api/user/role/getUserNum", "GET", { roleId });
 };
 //添加权限
 export const insertAuthority = (authorityName: string) => {
-  return hyRequest.request("/api/forum/user/authority/insertAuthority", "POST", {
+  return hyRequest.request("/api/user/authority/insertAuthority", "POST", {
     authorityName,
   });
 };
 //查看所有权限
 export const getAllAuthority = () => {
-  return hyRequest.request("/api/forum/user/authority/getAllAuthority", "GET");
+  return hyRequest.request("/api/user/authority/getAllAuthority", "GET");
 };
 //删除权限
 export const deleteAuthority = (authorityId: number) => {
-  return hyRequest.request("/api/forum/user/authority/deleteAuthority", "DELETE", {
+  return hyRequest.request("/api/user/authority/deleteAuthority", "DELETE", {
     authorityId,
   });
 };
 //添加接口
 export const insertInterface = (authorityId: number, interfacePath: string) => {
-  return hyRequest.request("/api/forum/user/interface/insertInterface", "POST", {
+  return hyRequest.request("/api/user/interface/insertInterface", "POST", {
     authorityId,
     interfacePath,
   });
 };
 //查询权限下的接口
 export const selectInterface = (authorityId: number) => {
-  return hyRequest.request("/api/forum/user/interface/selectByAuthority", "GET", {
+  return hyRequest.request("/api/user/interface/selectByAuthority", "GET", {
     authorityId,
   });
 };
 //删除接口
 export const deleteInterface = (interfaceId: number) => {
-  return hyRequest.request("/api/forum/user/interface/deleteInterface", "DELETE", {
+  return hyRequest.request("/api/user/interface/deleteInterface", "DELETE", {
     interfaceId,
   });
 };
 //重新加载角色权限
 export const updateRoleAuthority = () => {
-  return hyRequest.request("/api/forum/user/interface/updateRoleAuthority", "PUT");
+  return hyRequest.request("/api/user/interface/updateRoleAuthority", "PUT");
 };
 
 //给角色添加权限
 export const insertRoleAuthority = (authorityId: number, roleId: number) => {
-  return hyRequest.request("/api/forum/user/role/insertRoleAuthority", "POST", {
+  return hyRequest.request("/api/user/role/insertRoleAuthority", "POST", {
     authorityId,
     roleId,
   });
 };
 //给角色删除权限
 export const deleteRoleAuthority = (authorityId: number, roleId: number) => {
-  return hyRequest.request("/api/forum/user/role/deleteRoleAuthority", "DELETE", {
+  return hyRequest.request("/api/user/role/deleteRoleAuthority", "DELETE", {
     authorityId,
     roleId,
   });
--- service/rotation.ts
+++ service/rotation.ts
@@ -1,15 +1,17 @@
 import hyRequest from "./forum";
 //查看轮播图
-export const selectCarousel = ()=>{
-    return hyRequest.request("/api/user/carousel/selectAllCarousel","GET")
-}
+export const selectCarousel = () => {
+  return hyRequest.request("/api/user/carousel/selectAllCarousel", "GET");
+};
 //删除轮播图
-export const deleteCarousel = (carouselId:number)=>{
-    return hyRequest.request("/api/user/carousel/deleteCarousel","DELETE",{carouselId})
-}
+export const deleteCarousel = (carouselId: number) => {
+  return hyRequest.request("/api/user/carousel/deleteCarousel", "DELETE", {
+    carouselId,
+  });
+};
 //添加轮播
 export const addCarousel = (query: any, params: FormData) => {
-    return hyRequest.request("/api/user/carousel/insertCarousel","POST", query, {
-      body: params
-    });
-  };
+  return hyRequest.request("/api/user/carousel/insertCarousel", "POST", query, {
+    body: params,
+  });
+};
